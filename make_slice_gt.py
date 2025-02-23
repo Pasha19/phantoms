bdd6047d4e83a3b22113400c42ce565407a4896c
Pasha19 <20466125+Pasha19@users.noreply.github.com>
2025-02-23 16:06:47 +0300

    padding and new resampling

--- make_slice_gt.py
+++ make_slice_gt.py
@@ -13,10 +13,8 @@ import numpy as np
 import psutil
 import tifffile
 from gecatsim.pyfiles.GetMu import GetMu
-from PIL import Image
+from scipy import ndimage
 from tqdm import tqdm
-
-from make_phantom import resampled_volume
 
 
 def cfg_param_val_to_str(value) -> str:
@@ -131,6 +129,12 @@ def gen_geometry_in_slice_corection_matrix(
     return slice_scale
 
 
+def resample_volume(volume: np.ndarray, voxel_size: tuple[float, float, float], new_voxel_size) -> np.ndarray:
+    zoom = list([voxel_size[i] / new_voxel_size for i in range(3)])
+    volume_resampled = ndimage.zoom(volume, zoom=zoom, grid_mode=True, mode="grid-constant", order=1, prefilter=False)
+    return volume_resampled
+
+
 @dataclass
 class SliceProjWorkerArgs:
     xcist_tmp_path: pathlib.Path
@@ -184,7 +188,7 @@ def set_stdout_to_devnull() -> None:
     sys.stderr = devnull
 
 
-def create_gt_sliced_thin(ex: xc.CatSim, d: float, thin_scale: float = 0.05) -> np.ndarray:
+def create_gt_sliced_thin(ex: xc.CatSim, thin_scale: float = 0.05) -> np.ndarray:
     phantom_cfg_json = pathlib.Path(ex.cfg.phantom.filename)
     with open(phantom_cfg_json, "r") as f:
         phantom_cfg = json.load(f)
@@ -192,8 +196,7 @@ def create_gt_sliced_thin(ex: xc.CatSim, d: float, thin_scale: float = 0.05) -> 
     det_pix_size = ex.cfg.scanner.detectorRowSize
     sid = ex.cfg.scanner.sid
     sdd = ex.cfg.scanner.sdd
-    # voxel_size = det_pix_size / (sdd / sid)
-    voxel_size = d / ex.cfg.scanner.detectorColCount
+    voxel_size = det_pix_size / (sdd / sid)
     for i in range(phantom_cfg["n_materials"]):
         volume_file = str(phantom_cfg_json.parent / phantom_cfg["volumefractionmap_filename"][i])
         rows, cols, slices = (
@@ -209,17 +212,16 @@ def create_gt_sliced_thin(ex: xc.CatSim, d: float, thin_scale: float = 0.05) -> 
             phantom_cfg["y_size"][i],
             phantom_cfg["z_size"][i],
         )
-        size = y_size * phantom_cfg["cols"][i]
-        new_size_in_vox = int(size / voxel_size + 0.5)
-        if new_size_in_vox != volume.shape[1]:
-            volume, y_size = resampled_volume(volume, y_size, z_size, new_size_in_vox)
+        if y_size != voxel_size:
+            volume = resample_volume(volume, (x_size, y_size, z_size), voxel_size)
         volumes.append(volume)
+        new_size_in_vox = volume.shape[1]
     tmp_path = phantom_cfg_json.parent / f"tmp.{os.getpid()}"
     tmp_path.mkdir(exist_ok=True, parents=True)
-    vol_gt = np.empty(
+    vol_gt = np.zeros(
         (
             ex.cfg.scanner.detectorRowCount,
-            new_size_in_vox,
+            ex.cfg.scanner.detectorColCount,
             ex.cfg.scanner.detectorColCount,
         ),
         dtype=np.float32,
@@ -231,12 +233,9 @@ def create_gt_sliced_thin(ex: xc.CatSim, d: float, thin_scale: float = 0.05) -> 
         f.write(cfg_to_str(ex))
     process_count = psutil.cpu_count(logical=False)
     with Pool(processes=process_count, initializer=set_stdout_to_devnull) as pool:
-        # start_ind = max(0, (new_size_in_vox - ex.cfg.scanner.detectorColCount) // 2)
-        start_ind = 0
-        # end_ind = min(ex.cfg.scanner.detectorColCount, start_ind + new_size_in_vox)
-        end_ind = new_size_in_vox
-        # offset_ind = max(0, (ex.cfg.scanner.detectorColCount - new_size_in_vox) // 2)
-        offset_ind = 0
+        start_ind = max(0, (new_size_in_vox - ex.cfg.scanner.detectorColCount) // 2)
+        end_ind = min(ex.cfg.scanner.detectorColCount, start_ind + new_size_in_vox)
+        offset_ind = max(0, (ex.cfg.scanner.detectorColCount - new_size_in_vox) // 2)
         args = [
             SliceProjWorkerArgs(
                 xcist_tmp_path=xcist_tmp_path,
@@ -257,25 +256,20 @@ def create_gt_sliced_thin(ex: xc.CatSim, d: float, thin_scale: float = 0.05) -> 
     return vol_gt
 
 
+def xcist_from_config_path(cfg_path: pathlib.Path) -> xc.CatSim:
+    if cfg_path.is_dir():
+        return xc.CatSim(*list(map(str, cfg_path.glob("*.cfg"))))
+    return xc.CatSim(str(cfg_path))
+
+
 def main() -> None:
     root_path = pathlib.Path(__file__).parent.resolve()
-
-    cfg_path = root_path / "cfg"
-    xcist = xc.CatSim(
-        str(cfg_path / "phantom.cfg"),
-        str(cfg_path / "physics_flat.cfg"),
-        str(cfg_path / "protocol.cfg"),
-        str(cfg_path / "scanner_flat.cfg"),
-    )
+    cfg_path = root_path / "cfg_flat"
+    xcist = xcist_from_config_path(cfg_path)
     phantom_path = root_path / "phantoms/_test" / "phantom.json"
     xcist.cfg.phantom.filename = str(phantom_path)
-
-    vol_gt = create_gt_sliced_thin(xcist, d=501.7847226)
-
+    vol_gt = create_gt_sliced_thin(xcist)
     tifffile.imwrite(phantom_path.parent / "phantom.gt.tif", vol_gt, compression="zlib")
-    # vol_gt = (vol_gt - vol_gt.min()) / (vol_gt.max() - vol_gt.min())
-    # im = Image.fromarray(255 * vol_gt[vol_gt.shape[0] // 2, :, :]).convert("L")
-    # im.save(phantom_path.parent / "phantom.gt.png")
 
 
 if __name__ == "__main__":
